23a45a5c906e971f1fdf37662ea9dc50b2903bab
Saad Mirza <saadmirza009@gmail.com>
2018-08-18 23:50:52 +0500

    updated icons setting of sorting

--- src/components/MoviesTable.jsx
+++ src/components/MoviesTable.jsx
@@ -6,7 +6,6 @@ class MoviesTable extends Component {
     const sortColumn = this.props.sortColumn;
 
     if (sortColumn.path == path) {
-      console.log(sortColumn.order);
       sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
     } else {
       sortColumn.path = path;
@@ -15,15 +14,55 @@ class MoviesTable extends Component {
     this.props.onSort(sortColumn);
   };
   render() {
-    const { movies, onLike, onDelete } = this.props;
+    const { movies, onLike, onDelete, sortColumn } = this.props;
     return (
       <table className="table table-hover table-bordered">
         <thead>
           <tr>
-            <th onClick={() => this.raiseSort("title")}>Name</th>
-            <th onClick={() => this.raiseSort("genre.name")}>Genres</th>
-            <th onClick={() => this.raiseSort("numberInStock")}>Rank</th>
-            <th onClick={() => this.raiseSort("dailyRentalRate")}>Date</th>
+            <th onClick={() => this.raiseSort("title")}>
+              Name
+              <i
+                class={
+                  sortColumn.path === "title"
+                    ? "fa fa-sort-" + sortColumn.order
+                    : "fa fa-sort-desc"
+                }
+                aria-hidden="true"
+              />
+            </th>
+            <th onClick={() => this.raiseSort("genre.name")}>
+              Genres
+              <i
+                class={
+                  sortColumn.path == "genre.name"
+                    ? "fa fa-sort-" + sortColumn.order
+                    : "fa fa-sort-asc"
+                }
+                aria-hidden="true"
+              />
+            </th>
+            <th onClick={() => this.raiseSort("numberInStock")}>
+              Rank
+              <i
+                class={
+                  sortColumn.path == "numberInStock"
+                    ? "fa fa-sort-" + sortColumn.order
+                    : "fa fa-sort-asc"
+                }
+                aria-hidden="true"
+              />
+            </th>
+            <th onClick={() => this.raiseSort("dailyRentalRate")}>
+              Date
+              <i
+                class={
+                  sortColumn.path == "dailyRentalRate"
+                    ? "fa fa-sort-" + sortColumn.order
+                    : "fa fa-sort-asc"
+                }
+                aria-hidden="true"
+              />
+            </th>
             <th>Like</th>
             <th>Action</th>
           </tr>
